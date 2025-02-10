c5f08eb2ea09c3216e0ad3f5446ac02fd423769d
Cptzeph23 <asekazephaniah@gmail.com>
2025-02-10 10:18:07 +0300

    Initial Commit

--- cukcuapp/urls.py
+++ cukcuapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('choir/', views.choir, name='choir'),
     path('portfolio/', views.portfolio,name='portfolio'),
     path('portfoliodetails/', views.servicedetails, name='portfoliodetails'),
     path('servicedetails/', views.servicedetails, name='servicedetails'),
