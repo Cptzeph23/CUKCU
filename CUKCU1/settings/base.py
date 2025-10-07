0288a60cd6565fe7598a43cfaca51fded2fc9c25
Cptzeph23 <asekazephaniah@gmail.com>
2025-10-07 16:49:54 +0300

    whitenoise for static files

--- CUKCU1/settings/base.py
+++ CUKCU1/settings/base.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # Add optional middleware if packages are installed
