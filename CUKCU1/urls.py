b84f23fc4a2afcc826bfffdbfcb34b0d34076eb1
Cptzeph23 <asekazephaniah@gmail.com>
2025-10-10 01:08:47 +0300

    sitemap

--- CUKCU1/urls.py
+++ CUKCU1/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from healthz import health_check
-
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
@@ -12,6 +12,8 @@ urlpatterns = [
     path('', include('cukcuapp.urls')),
     path('healthz', health_check, name='healthz_no_slash'),  # Without slash
     path('health/', health_check, name='health'),  # Alternative path
+    path("robots.txt", RedirectView.as_view(url="/static/robots.txt")),
+    path("sitemap.xml", RedirectView.as_view(url="/static/sitemap.xml")),
 ]
 
 # Serve media files in production
