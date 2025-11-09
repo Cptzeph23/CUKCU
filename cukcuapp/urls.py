2d670ce6ae7652691c467337cbd53199a7a73a35
Cptzeph23 <asekazephaniah@gmail.com>
2025-11-09 23:16:46 +0300

    sitemap

--- cukcuapp/urls.py
+++ cukcuapp/urls.py
@@ -35,4 +35,5 @@ urlpatterns = [
     path('leaderboard/', views.leaderboard, name='leaderboard'),
     path('exec', views.exec, name='exec'),
     path("sitemap.xml", TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
