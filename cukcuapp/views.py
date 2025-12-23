a50a4c02dedb344ea20395d8208f80bfb2f53cc1
Kelly-emrys <emrysngone2005@gmail.com>
2025-12-23 15:18:15 +0300

    Change index view to render 'index.html'

--- cukcuapp/views.py
+++ cukcuapp/views.py
@@ -23,7 +23,7 @@ def healthz(request):
 
 # Create your views here.
 def index(request):
-    return render(request, 'maintenance.html')
+    return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
